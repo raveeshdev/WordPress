4d6882b3dc7a94d37023a92098eaaabd7417fb48
stacx-game <105474485+stacx-game@users.noreply.github.com>
2022-05-13 10:23:47 +0530

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -37,6 +37,14 @@ define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+
+
+define('FORCE_SSL_ADMIN', true);
+// in some setups HTTP_X_FORWARDED_PROTO might contain 
+// a comma-separated list e.g. http,https
+// so check for https existence
+if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
+       $_SERVER['HTTPS']='on';
 /**#@+
  * Authentication unique keys and salts.
  *
